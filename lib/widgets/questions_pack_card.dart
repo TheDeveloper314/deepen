10f14e9f41adc1266dda34c1ca305c9d0bd49016
thedeveloper314 <Tariqwaleed27@gmail.com>
2025-09-19 17:40:29 +0300

    changed images paths

--- lib/widgets/questions_pack_card.dart
+++ lib/widgets/questions_pack_card.dart
@@ -43,7 +43,7 @@ class QuestionsPackCard extends ConsumerWidget {
                   color: AppTheme.primary,
                 ),
                 child: Image.asset(
-                  "images/${questionsPack.packImage}.png",
+                  "assets/images/${questionsPack.packImage}.png",
                   errorBuilder: (context, obj, s) {
                     return Text("Error");
                   },
